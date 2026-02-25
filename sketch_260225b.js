a5c7a5458a32314a834deabd51bcba6d78e67868
cybunetics <cybunetics@gmail.com>
2026-02-25 09:04:25 -0800

    added code

---
+++ sketch_260225b.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas (500, 500);
+  background(0);
+}
+
+
+function draw() {
+  noStroke();
+
+
+
+  fill(150);//ears
+  ellipse (width/2 - 50, height/3, 50, 150);
+  ellipse (width/2 + 50, height/3, 50, 150);
+
+  fill(255, 204, 250); //pink ear
+  ellipse (width/2 - 50, height/3, 10, 100);
+  ellipse (width/2 + 50, height/3, 10, 100);
+
+  fill(150);//circle head
+  circle(width/2, height/2, 200);
+
+  fill(255);//eyepatch + frown
+  stroke(0);
+  strokeWeight(5);
+  line(120, 80, 340, 300);
+
+  noFill();
+  arc(250, 305, 60, 60, 1, PI);
+
+
+  fill(180);//eyes
+  circle(width/2 - 50, height/2 + 10, 50);
+  fill(0);
+  circle(width/2 + 50, height/2 + 10, 50);
+}
